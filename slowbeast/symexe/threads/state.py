c53c264538d7808bf55ef1083e509ea6cb01c8ad
xshandil <xshandil@fi.muni.cz>
2024-11-05 13:26:44 +0100

    slots and other fixes.

--- slowbeast/symexe/threads/state.py
+++ slowbeast/symexe/threads/state.py
@@ -1,23 +1,21 @@
-
 from __future__ import annotations
 from sys import stdout
 from typing import TextIO, Self
 
 from slowbeast.core.callstack import CallStack
 from slowbeast.core.errors import GenericError
-from slowbeast.core.state import ExecutionState
-from slowbeast.domains.concrete_bitvec import ConcreteBitVec
-from slowbeast.domains.expr import Expr
-from slowbeast.domains.pointer import Pointer
-from slowbeast.ir.instruction import Alloc, GlobalVariable, Instruction, ThreadJoin, ValueInstruction
+from slowbeast.ir.instruction import ThreadJoin
 from slowbeast.symexe.state import SEState as BaseState, Thread, Event
 from slowbeast.symexe.threads.iexecutor import IExecutor
-from slowbeast.util.debugging import ldbgv
 
 
 class TSEState(BaseState):
-    """TODO: Deprecate all things which shouldn't be owned by the state but should only be owned by a specific thread:
-    state.pc, _current_thread,"""
+    """
+    TODO: Deprecate all things which shouldn't be owned by
+    the state but should only be owned by a specific thread:
+    (state.pc, _current_thread)
+    """
+
     __slots__ = (
         "_threads",
         "_current_thread",
@@ -26,40 +24,43 @@ class TSEState(BaseState):
         "_wait_mutex",
         "_mutexes",
         "_last_tid",
-        "_events",
         "conflicts",
-        "_tainted_locations",
-        "_race_alert"
+        "immediate_conflicts",
+        "data_race",
+        "causes",
+        "caused_by",
+        "transition",
+        "is_bot",
     )
 
     def __init__(
-        self, executor : IExecutor, pc=None, m=None, solver=None, constraints=None
+        self, executor: IExecutor, pc=None, m=None, solver=None, constraints=None
     ) -> None:
         super().__init__(executor, pc, m, solver, constraints)
         self._last_tid = 0
         self._current_thread = 0
         # [Thread(0, pc, self.memory.get_cs() if m else None)]
         if m:
-            self._threads = { 0: Thread(0, pc, self.memory.get_cs())}
+            self._threads = {0: Thread(0, pc, self.memory.get_cs())}
         else:
             None
         self._wait_join = {}
         self._exited_threads = {}
         self._mutexes = {}
         self._wait_mutex = {}
-        self.conflicts : set[Self] = set()
-        self.immediate_conflicts : set[Self] = set()
-        self._tainted_locations = []
-        self._race_alert = False
+        self.conflicts: set[Self] = set()
+        self.immediate_conflicts: set[Self] = set()
         self.is_bot = False
-        self.causes : set[Self] = set()
-        self.caused_by : Self = self # Only bottom event can self reference itself.
-        self.transition : Transition | None = None # type: ignore # None for bottom event
-        self.data_race : bool = False
-        self.bottom : Self | None = None # Bottom Event. One per instance. Kinda redundant but OK.
+        self.causes: set[Self] = set()
+        self.caused_by: Self = self  # Only bottom event can self reference itself.
+        self.transition: Transition | None = None  # noqa:F821 None for bottom event
+        self.data_race: bool = False
+        # self.bottom: Self | None = (
+        #     None  # Bottom Event. One per instance. Kinda redundant but OK.
+        # )
 
     def _thread_idx(self, thr: Thread) -> int:
-        '''Return ID of a given thread. Thread's own ID'''
+        """Return ID of a given thread. Thread's own ID"""
         for idx in self._threads:
             if self._threads[idx] == thr:
                 return idx
@@ -67,9 +68,9 @@ class TSEState(BaseState):
             return None
 
     def _copy_to(self, new: Self) -> None:
-        assert not self.is_bot, "Don't copy the bottom state"
+        # assert not self.is_bot, "Don't copy the bottom state"
         super()._copy_to(new)
-        new._threads = { id: thr.copy() for id,thr in self._threads.items() }
+        new._threads = {id: thr.copy() for id, thr in self._threads.items()}
         new._wait_join = self._wait_join.copy()
         new._exited_threads = self._exited_threads.copy()
         new._last_tid = self._last_tid
@@ -78,9 +79,8 @@ class TSEState(BaseState):
         new._wait_mutex = {mtx: W.copy() for mtx, W in self._wait_mutex.items() if W}
         new._race_alert = self._race_alert
         new.conflicts = self.conflicts
-
-    def trace(self):
-        return self._event_trace
+        if self.is_bot:
+            new.is_bot = True
 
     # def lazy_eval(self, v: Union[Alloc, GlobalVariable]):
     #     value = self.try_eval(v)
@@ -110,10 +110,10 @@ class TSEState(BaseState):
             self._threads[self._current_thread].pc = self.pc
 
     def sync_cs(self) -> None:
-        """ Synchronise callstack"""
+        """Synchronise callstack"""
         if self._threads:
             self.thread().cs = self.memory.get_cs()
-    
+
     def add_event(self) -> None:
         self._events.append(Event(self))
 
@@ -156,22 +156,18 @@ class TSEState(BaseState):
         return self._threads[self._current_thread if idx is None else idx].get_id()
 
     def pause_thread(self, idx=None) -> None:
-        # self._trace.append(f"pause thread {self.thread(idx).get_id()}")
         self._threads[self._current_thread if idx is None else idx].pause()
 
     def unpause_thread(self, idx=None) -> None:
-        # self._trace.append(f"unpause thread {self.thread(idx).get_id()}")
         self._threads[self._current_thread if idx is None else idx].unpause()
 
     def start_atomic(self, idx=None) -> None:
-        # self._trace.append(f"thread {self.thread(idx).get_id()} begins atomic sequence")
         assert not self._threads[
             self._current_thread if idx is None else idx
         ].in_atomic()
         self._threads[self._current_thread if idx is None else idx].set_atomic(True)
 
     def end_atomic(self, idx=None) -> None:
-        # self._trace.append(f"thread {self.thread(idx).get_id()} ends atomic sequence")
         assert self._threads[self._current_thread if idx is None else idx].in_atomic()
         self._threads[self._current_thread if idx is None else idx].set_atomic(False)
 
@@ -188,13 +184,11 @@ class TSEState(BaseState):
         return mtx in self._mutexes
 
     def mutex_lock(self, mtx, idx=None) -> None:
-        # self._trace.append(f"thread {self.thread(idx).get_id()} locks mutex {mtx}")
         tid = self.thread(self._current_thread if idx is None else idx).get_id()
         assert self.mutex_locked_by(mtx) is None, "Locking locked mutex"
         self._mutexes[mtx] = tid
 
     def mutex_unlock(self, mtx, idx=None) -> None:
-        # self._trace.append(f"thread {self.thread(idx).get_id()} unlocks mutex {mtx}")
         assert (
             self.mutex_locked_by(mtx)
             == self.thread(self._current_thread if idx is None else idx).get_id()
@@ -210,8 +204,6 @@ class TSEState(BaseState):
 
     def mutex_wait(self, mtx, idx=None) -> None:
         "Thread idx waits for mutex mtx"
-
-        # self._trace.append(f"thread {self.thread(idx).get_id()} waits for mutex {mtx}")
         tid = self._current_thread if idx is None else idx
         assert self.mutex_locked_by(mtx) is not None, "Waiting for unlocked mutex"
         self.pause_thread(idx)
@@ -220,14 +212,11 @@ class TSEState(BaseState):
     def exit_thread(self, retval, tid=None) -> None:
         """Exit thread and wait for join (if not detached)"""
         tid = self._current_thread if tid is None else tid
-        # self._trace.append(f"exit thread {tid} with val {retval}")
         assert tid not in self._exited_threads
         self._exited_threads[tid] = retval
         self.remove_thread(tid)
 
         if tid in self._wait_join:
-            # self._trace.append(f"thread {tid} was waited for by {self._wait_join[tid]}")
-            # idx's of the threads that are waiting on 'tid' to exit
             waiting_thread_ids = self._wait_join.pop(tid)
             for waitidx in waiting_thread_ids:
                 assert self.thread(waitidx).is_paused(), self._wait_join
@@ -243,17 +232,11 @@ class TSEState(BaseState):
         tid: id of the thread that joins
         totid: id of the thread to which to join (None means the current thread)
         """
-        # self._trace.append(
-        #    f"join thread {tid} to {self.thread().get_id() if totid is None else totid}"
-        # )
         if tid in self._exited_threads:
             # pass the return value
             retval = self._exited_threads.pop(tid)
             self.set(self.pc, retval)
             self.pc = self.pc.get_next_inst()
-            # self._trace.append(
-            #    f"thread {tid} waited for a join, joining with val {retval}"
-            # )
             return
 
         toidx = self._current_thread if totid is None else self._thread_idx(totid)
@@ -264,16 +247,12 @@ class TSEState(BaseState):
         self.pause_thread(toidx)
 
     def remove_thread(self, idx=None) -> None:
-        # self._trace.append(f"removing thread {self.thread(idx).get_id()}")
         self._threads.pop(self._current_thread if idx is None else idx)
-        # schedule thread 0 (if there is any)
         if self._threads:
             self.pc = self._threads[0].pc
             self.memory.set_cs(self._threads[0].cs)
             self._current_thread = 0
 
-        # after the last thread terminates, exit(0) is called
-        # see man pthread_exit
         if self.num_threads() == 0:
             self.set_exited(0)
 
@@ -281,7 +260,7 @@ class TSEState(BaseState):
         return len(self._threads)
 
     def race_condition_possible(self) -> bool:
-        """ Sets `race_alert` flag if more than 1 thread is active."""
+        """Sets `race_alert` flag if more than 1 thread is active."""
         sleeping_threads = [thread.is_paused() for thread in self._threads.values()]
         if all(sleeping_threads):
             self.set_error(GenericError("Deadlock detected"))
@@ -290,7 +269,7 @@ class TSEState(BaseState):
         if not self._race_alert:
             self._tainted_locations = []
         return self._race_alert
-        
+
     def threads(self) -> iter:
         return self._threads.values()
 
@@ -317,6 +296,4 @@ class TSEState(BaseState):
         return output_states
 
     def makeBottom(self) -> None:
-        # TODO
         self.is_bot = True
-        
